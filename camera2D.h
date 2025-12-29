029007be321658fdddef75b0e5e3126852c5dba1
dungemeister <yuriy.vazha@gmail.com>
2025-12-29 21:47:27 +0400

    Updated camera class

--- camera2D.h
+++ camera2D.h
@@ -1,39 +1,56 @@
 #pragma once
 #include "vector2d.h"
 
-struct Camera2D{
-    Camera2D(int width, int height)
-    :zoom(1.f)
-    ,viewport_width(width)
-    ,viewport_height(height)
-    ,world_center(width/2, height/2)
-    {
+struct Camera2D {
+    // camera's center в world pixels
+    float cx = 0.f;   
+    float cy = 0.f;
+    // camera's zoom
+    float zoom = 1.f;
 
+    int viewport_width = 1;    // размеры world viewport
+    int viewport_height = 1;
+
+    inline SDL_FPoint world_to_screen(SDL_FPoint w)
+    {
+        const float hx = viewport_width * 0.5f;
+        const float hy = viewport_height * 0.5f;
+        return SDL_FPoint{
+            (w.x - cx) * zoom + hx,
+            (w.y - cy) * zoom + hy
+        };
     }
 
-    Camera2D()
-    :Camera2D(1920, 1080)
-    {}
+    static inline SDL_FPoint screen_to_world(const Camera2D& cam, SDL_FPoint pos)
+    {
+        const float hx = cam.viewport_width * 0.5f;
+        const float hy = cam.viewport_height * 0.5f;
+        return SDL_FPoint{
+            (pos.x - hx) / cam.zoom + cam.cx,
+            (pos.y - hy) / cam.zoom + cam.cy
+        };
+    }
 
-    float zoom;
-    int viewport_width;
-    int viewport_height;
-    Vector2D world_center;
+    static inline bool in_rect(int x, int y, const SDL_Rect& r)
+    {
+        return x >= r.x && x < r.x + r.w && y >= r.y && y < r.y + r.h;
+    }
+
+    inline void zoom_at_mouse(const SDL_Rect& worldVP,
+                                int mouseX, int mouseY, float zoomMul)
+    {
+        if (!in_rect(mouseX, mouseY, worldVP)) return;
+
+        SDL_FPoint mInVP { float(mouseX - worldVP.x), float(mouseY - worldVP.y) };
+
+        const SDL_FPoint before = screen_to_world(*this, mInVP);
+
+        zoom *= zoomMul;
+        zoom = std::clamp(zoom, 0.5f, 4.0f);
+
+        const SDL_FPoint after = screen_to_world(*this, mInVP);
+
+        cx += (before.x - after.x);
+        cy += (before.y - after.y);
+    }
 };
-
-inline Vector2D world_to_screen(const Camera2D& cam, Vector2D world){
-    const Vector2D half{ cam.viewport_width * 0.5f, cam.viewport_height * 0.5f };
-    return {
-        (world.x - cam.world_center.x) * cam.zoom + half.x,
-        (world.y - cam.world_center.y) * cam.zoom + half.y
-    };
-}
-
-inline Vector2D ScreenToWorld(const Camera2D& cam, Vector2D screen)
-{
-    const Vector2D half{ cam.viewport_width * 0.5f, cam.viewport_height * 0.5f };
-    return {
-        (screen.x - half.x) / cam.zoom + cam.world_center.x,
-        (screen.y - half.y) / cam.zoom + cam.world_center.y
-    };
-}
